c030c4993534ecbf560f43d5f88353bc2513d0d6
Arun <coolarun477@gmail.com>
2017-06-30 12:28:46 -0700

    Update script1.js

--- javascripts/script1.js
+++ javascripts/script1.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
    $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
     FB.init({
       appId: '168329377033776',
-      version: 'v2.9' // or v2.1, v2.2, v2.3, ...
+      version: 'v2.7' // or v2.1, v2.2, v2.3, ...
     });     
     $('#loginbutton,#feedbutton').removeAttr('disabled');
     
