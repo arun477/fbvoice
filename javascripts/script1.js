cd798794e8c8999d406d7c2451e753593efad838
Arun <coolarun477@gmail.com>
2017-07-04 09:54:55 -0700

    hell

--- javascripts/script1.js
+++ javascripts/script1.js
@@ -81,7 +81,7 @@ $(document).ready(function(){
   
   updateStatusCallback = function(response) {
   if (response.status === 'connected') {
-    FB.login(function(response) {
+     FB.login(function(response) {
       $.userDetails = response;
         if (response.authResponse) {
             console.log('Access Token: ' + response.authResponse.accessToken);
@@ -89,7 +89,6 @@ $(document).ready(function(){
             console.log('User cancelled login or did not fully authorize.');
         }
     });
-  }
     console.log('Logged in.');
      FB.api('/me', {fields: 'first_name,name,picture.type(large),cover,age_range,gender,birthday'}, function(response) {
   console.log(response);
