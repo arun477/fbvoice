8cbe2f7334da33c5a440a0a062de2588ecd8135f
Arun <coolarun477@gmail.com>
2017-07-04 23:52:27 -0700

    hh

--- javascripts/script1.js
+++ javascripts/script1.js
@@ -5,13 +5,13 @@
 //--------------------------------------------
  //sdk for google map api
 
-   var map, infoWindow;
-     function initMap() {
-        map = new google.maps.Map(document.getElementById('item3-c'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 6
-        });
-        infoWindow = new google.maps.InfoWindow;
+ var map, infoWindow;
+ function initMap() {
+  map = new google.maps.Map(document.getElementById('item3-c'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 6
+  });
+  infoWindow = new google.maps.InfoWindow;
 
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
@@ -37,8 +37,8 @@
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
+          'Error: The Geolocation service failed.' :
+          'Error: Your browser doesn\'t support geolocation.');
         infoWindow.open(map);
       } 
  // $("body").append(" <script async defer src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDyofQaKTXm33Xy3iVkJr3j6xh818dfftg&callback=initMap'></script>");
@@ -56,16 +56,16 @@ $(document).ready(function(){
   //map api section
 
 
-    
-   
-   $("body").append(" <script async defer src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDyofQaKTXm33Xy3iVkJr3j6xh818dfftg&callback=initMap'></script>");
-    
+
+
+  $("body").append(" <script async defer src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDyofQaKTXm33Xy3iVkJr3j6xh818dfftg&callback=initMap'></script>");
+
 
   //--------------------------------------
   //facebook api section 
- 
+
   
-    var responses;
+  var responses;
    // this initialize the facebook sdk and cache it locally
    $.ajaxSetup({ cache: true});
    $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
@@ -78,74 +78,74 @@ $(document).ready(function(){
     
   });
 
-  
-  updateStatusCallback = function(response) {
-  if (response.status === 'connected') {
+
+   updateStatusCallback = function(response) {
+    if (response.status === 'connected') {
      FB.login(function(response) {
       $.userDetails = response;
-        if (response.authResponse) {
-            console.log('Access Token: ' + response.authResponse.accessToken);
-        } else {
-            console.log('User cancelled login or did not fully authorize.');
-        }
+      if (response.authResponse) {
+        console.log('Access Token: ' + response.authResponse.accessToken);
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
     });
-    console.log('Logged in.');
+     console.log('Logged in.');
      FB.api('/me', {fields: 'first_name,name,picture.type(large),cover,age_range,gender,birthday'}, function(response) {
-  console.log(response);
-   $.responses = response;
-   $.url = response.picture.data.url;
-   $.nn = response.name;
-   $.cov  = response.cover.source;
-   $.age1 = response.age_range.min;
-   $.gen = response.gender;
-   $.birth = response.birthday;
-   $.birth = $.birth.split("/");
-   $.birth = $.birth[2];
-   $.birth = parseInt($.birth);
-   $.ageday = 2017 - $.birth;
-   $("#loginbutton").hide();
-   $("#user").show();
-   $("#logoutbutton").show();
-   $("#username").text($.responses.first_name);
-   $("#profileimg").attr("src",$.url);
-   $("#name").text($.nn);
-       $("#prof1").attr("src",$.url);
-       $("#profession").text($.ff);
-       $("#item3-a").css("background-image",'url('+$.cov+')');
-       $("#age").text($.ageday);
-       if($.gen ==="male"){
+      console.log(response);
+      $.responses = response;
+      $.url = response.picture.data.url;
+      $.nn = response.name;
+      $.cov  = response.cover.source;
+      $.age1 = response.age_range.min;
+      $.gen = response.gender;
+      $.birth = response.birthday;
+      $.birth = $.birth.split("/");
+      $.birth = $.birth[2];
+      $.birth = parseInt($.birth);
+      $.ageday = 2017 - $.birth;
+      $("#loginbutton").hide();
+      $("#user").show();
+      $("#logoutbutton").show();
+      $("#username").text($.responses.first_name);
+      $("#profileimg").attr("src",$.url);
+      $("#name").text($.nn);
+      $("#prof1").attr("src",$.url);
+      $("#profession").text($.ff);
+      $("#item3-a").css("background-image",'url('+$.cov+')');
+      $("#age").text($.ageday);
+      if($.gen ==="male"){
        $("#gender").attr("src","images/maleicon.png");
      } else{
        $("#gender").attr("src","images/femaleicon.png");
      }
-  
-});
-  }
-  else {
+
+   });
+   }
+   else {
     //FB.login(function(){}, {scope: 'publish_actions'});
-     FB.login(function(response) {
+    FB.login(function(response) {
       $.userDetails = response;
-        if (response.authResponse) {
-            console.log('Access Token: ' + response.authResponse.accessToken);
-        } else {
-            console.log('User cancelled login or did not fully authorize.');
-        }
+      if (response.authResponse) {
+        console.log('Access Token: ' + response.authResponse.accessToken);
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
     });
   }
 }
 
-  $("#loginbutton").click(function(){
-  	FB.getLoginStatus(updateStatusCallback);
+$("#loginbutton").click(function(){
+ FB.getLoginStatus(updateStatusCallback);
 
-    
 
-  });
 
-  
+});
 
-  $("#logoutbutton").click(function(){
-    FB.logout();
-  });
+
+
+$("#logoutbutton").click(function(){
+  FB.logout();
+});
 
 
 //--------------------------------------------------
@@ -155,60 +155,60 @@ $("#micke1").click(function(){
 
   if (window.hasOwnProperty('webkitSpeechRecognition')) {
 
-      var recognition = new webkitSpeechRecognition();
+    var recognition = new webkitSpeechRecognition();
 
-      recognition.continuous = false;
-      recognition.interimResults = false;
+    recognition.continuous = false;
+    recognition.interimResults = false;
 
-      recognition.lang = "en-US";
-      recognition.start();
+    recognition.lang = "en-US";
+    recognition.start();
 
-      recognition.onresult = function(e) {
-        $.result = e.results[0][0].transcript
-       console.log($.result);
-       $.resultparts = $.result.split(" ");
-       console.log($.resultparts);
-       if($.resultparts[0]==="post" || $.resultparts[0]==="posts" || $.resultparts[0]==="past" || $.resultparts[0]==="first"){
+    recognition.onresult = function(e) {
+      $.result = e.results[0][0].transcript
+      console.log($.result);
+      $.resultparts = $.result.split(" ");
+      console.log($.resultparts);
+      if($.resultparts[0]==="post" || $.resultparts[0]==="posts" || $.resultparts[0]==="past" || $.resultparts[0]==="first"){
         $.resultparts.shift();
         $.ree = $.resultparts.join(" ");
         FB.login(function(){
-  FB.api('/me/feed', 'post', {message: $.ree});
-}, {scope: 'publish_actions'});
-      
+          FB.api('/me/feed', 'post', {message: $.ree});
+        }, {scope: 'publish_actions'});
+
         
         $("#item4-b").html("<h1 id='post'>" +"message : " +" " +$.ree +" " + "has been successfully posted"+"</h1>" );
-         FB.getLoginStatus(updateStatusCallback2);
-       } else if($.resultparts[0]==="feed" || $.resultparts[0]==="feet" || $.resultparts[0]==="field" || $.resultparts[0]==="set" || $.resultparts[0]==="speed" || $.resultparts[0]==="Shahid" || $.resultparts[0]==="feel" || $.resultparts[0]==="seed" || $.resultparts[0]==="read" || $.resultparts[0]==="food"){
-              FB.api(
-  '/me',
-  'GET',
-  {"fields":"posts.limit(3){from,message}"},
-  function(response) {
-     console.log(response);
-     $.fee = response.posts.data;
+        FB.getLoginStatus(updateStatusCallback2);
+      } else if($.resultparts[0]==="feed" || $.resultparts[0]==="feet" || $.resultparts[0]==="field" || $.resultparts[0]==="set" || $.resultparts[0]==="speed" || $.resultparts[0]==="Shahid" || $.resultparts[0]==="feel" || $.resultparts[0]==="seed" || $.resultparts[0]==="read" || $.resultparts[0]==="food"){
+        FB.api(
+          '/me',
+          'GET',
+          {"fields":"posts.limit(3){from,message}"},
+          function(response) {
+           console.log(response);
+           $.fee = response.posts.data;
 
-     for (let i in $.fee){
-    
-      $("#item4-b").html("<h2 class='feed'>" + $.fee[i].from.name +" "+":"+ $.fee[i].message +"</h2>");
+           for (let i in $.fee){
+            $("#vid").hide();
+            $("#item4-b").append("<h2 class='feed'>" + $.fee[i].from.name +" "+":"+ $.fee[i].message +"</h2>");
 
-     }
-     
-  }
-);
-       }
-        recognition.stop();
-        
-      };
+          }
 
-      recognition.onerror = function(e) {
-        recognition.stop();
+        }
+        );
       }
+      recognition.stop();
 
+    };
+
+    recognition.onerror = function(e) {
+      recognition.stop();
     }
+
+  }
 });
 
 // for other events
- $("#item4-c").hide();
+$("#item4-c").hide();
 
 $("#cmdlist").click(function(){
   $("#item4-c").slideToggle(1500);
@@ -218,9 +218,9 @@ $("#cmdlist").click(function(){
 updateStatusCallback2 = function(response) {
   if (response.status === 'connected') {
     console.log('Logged in.');
-     
-     FB.api('/me/feed', 'post', {message: $.ree});   
-  
+
+    FB.api('/me/feed', 'post', {message: $.ree});   
+
 
   }
   else {
@@ -233,12 +233,12 @@ updateStatusCallback2 = function(response) {
 
 
 
- $("#gender").click(function(){
-    
+$("#gender").click(function(){
 
-    
 
-  });
+
+
+});
 
 
 });
